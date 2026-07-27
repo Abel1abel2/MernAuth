5150fc0d7dd605f394a6567a60f5ae4e224a62f0
abel <abellegend4321@gmail.com>
2026-07-27 15:44:44 +0300

    updating the option

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,12 @@ app.use(cors({
 }));
 
 // Crucial: Handle preflight (OPTIONS) requests before any routes
-app.options('*', cors()); 
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return cors()(req, res, next);
+  }
+  next();
+}); 
 
 app.use(cookieParser())
 
